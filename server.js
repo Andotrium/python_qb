4020920cec84a794ef80e559eece9531fdf0b9c7
Jagjit0306 <146818104+Jagjit0306@users.noreply.github.com>
2024-05-14 02:57:14 +0530

    added post req handling for new question

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
 
 const db = require('./models/db.js')
 
 dotenv.config()
 mongoose.connect(process.env.database1_key)
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.listen(6969)
 app.get('/', (req, res)=>{
-    res.send('yo wassup')
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/newq', async function(req, res) {
+    const newq = await db.create({
+        ques: req.body.ques,
+        op1: req.body.op1,
+        op2: req.body.op2,
+        op3: req.body.op3,
+        op4: req.body.op4,
+        submittedBy: req.body.submittedBy,
+        corr: 0,
+        incorr: 0
+    })
 })
