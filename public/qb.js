ab30364629b47bc68b7f4e05a812ad672baf4b9a
Pratham <59961114+Andotrium@users.noreply.github.com>
2024-05-14 14:05:05 +0530

    questions page

--- public/qb.js
+++ public/qb.js
@@ -5,11 +5,12 @@ let db = [{
     "_id": {
       "$oid": "66428a3d01758d952d1a2502"
     },
-    "ques": "The first question",
+    "ques": "The first question Pratham Andotra",
     "op1": "option 1",
     "op2": "option2",
     "op3": "option 3",
     "op4": "option 4",
+    "correct": 2,
     "submittedBy": "Pratham"
   },
   {
@@ -24,6 +25,7 @@ let db = [{
     "op2": "option2",
     "op3": "option 3",
     "op4": "option 4",
+    "correct": 4,
     "submittedBy": "Pratham"
   },
   {
@@ -38,21 +40,80 @@ let db = [{
     "op2": "option2",
     "op3": "option 3",
     "op4": "option 4",
+    "correct": 1,
     "submittedBy": "Pratham"
   }]
+  // console.log(db[0])
+var len = db.length
+var visited_q = []
 
-  var length = db.length
-  var q_visited = []
 
-function q_select(){
-    var present_q = Math.floor(Math.random()*length)
-    if(!q_visited.contains(present_q)){
-        q_visited.push(present_q);
-        db[present_q];
+function display(){
+  for(let x=0;x<4;x++){
+    document.getElementsByClassName('option')[x].classList.remove('red')
+    document.getElementsByClassName('option')[x].classList.remove('green')
+  }
+  var present_q = Math.floor(Math.random()*len)
+  if(!visited_q.includes(present_q)){
+    console.log(present_q)
+    console.log(db[present_q]['ques'])
+    document.getElementById('questions').getElementsByTagName('p')[0].innerHTML = db[present_q]['ques'];
+    document.getElementsByClassName('option')[0].innerHTML = db[present_q]['op1'];
+    document.getElementsByClassName('option')[1].innerHTML = db[present_q]['op2'];
+    document.getElementsByClassName('option')[2].innerHTML = db[present_q]['op3'];
+    document.getElementsByClassName('option')[3].innerHTML = db[present_q]['op4'];
+
+    document.getElementsByClassName('option')[0].addEventListener('click',(e)=>{
+      if(1 == db[present_q]['correct']){
+        document.getElementsByClassName('option')[0].classList.add('green')
+        setTimeout(display,500)
+      }
+      else {
+        document.getElementsByClassName('option')[0].classList.add('red')
+      }
+    })
+    document.getElementsByClassName('option')[1].addEventListener('click',(e)=>{
+      if(2 == db[present_q]['correct']){
+        document.getElementsByClassName('option')[1].classList.add('green')
+        setTimeout(display,500)
+      }
+      else {
+        document.getElementsByClassName('option')[1].classList.add('red')
+      }
+    })
+    document.getElementsByClassName('option')[2].addEventListener('click',(e)=>{
+      if(3 == db[present_q]['correct']){
+        document.getElementsByClassName('option')[2].classList.add('green')
+        setTimeout(display,500)
+        
+      }
+      else {
+        document.getElementsByClassName('option')[2].classList.add('red')
+      }
+    })
+    document.getElementsByClassName('option')[3].addEventListener('click',(e)=>{
+      if(4 == db[present_q]['correct']){
+        document.getElementsByClassName('option')[3].classList.add('green')
+        
+        setTimeout(display,1000)
+      }
+      else {
+        document.getElementsByClassName('option')[3].classList.add('red')
+      }
+    })
+    // document.getElementsByClassName('option')[1].addEventListener = db[present_q]['op4'];
+    // document.getElementsByClassName('option')[2].addEventListener = db[present_q]['op4'];
+    // document.getElementsByClassName('option')[3].addEventListener = db[present_q]['op4'];
+    
+  }
+    else{
+      display()
     }
-
 }
-document.getElementById('questions').innerHTML = db[0]['ques']
+
+display();
+
+
 
 // iterate between the elements of db, and set innerHTML of question and 
 // option divs to the respect elements under the dictionaries
